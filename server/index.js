cfaa9654e32d54aa6faf6010b606e6236def8e02
anna <a.sahradyan72@gmail.com>
2022-09-18 02:26:49 +0400

    create index js file

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const app = express();
-const db = require('./db');
-const Image = db.image;
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 app.use(fileUpload({
     createParentPath: true,
 }));
-app.post("/uploads", (req, res) => {
-    if (req.files) {
+app.post("/upload", (req, res) => {
+    if (!req.files) {
         return res.status(400).json({msg: "No file uploaded"})
     }
     const file = req.files.file;
     if (!file)
         return res.json({error: "Incorrect input name"});
     const newFileName = encodeURI(Date.now() + "-" + file.name);
-    file.mv(`${__dirname}/public/uploads/${newFileName}`, err => {
+    file.mv(`${__dirname}/testnode/public/uploads/${newFileName}`, err => {
         if (err) {
             console.log(err);
             return res.status(500).send(err);
@@ -28,20 +29,5 @@ app.post("/uploads", (req, res) => {
     });
 
 });
-// function writeToDb(data, res) {
-//     data = JSON.parse(data, true);
-//     console.log(data);
-//     Image.create({
-//         image_name: data['input-1'],
-//         file_name: data['input-2'],
-//         user_name : data['input-3'],
-//     })
-//         .then(result => {
-//             console.log(result);
-//             res.end('ok');
-//         }).catch(err => {
-//         console.log(err);
-//         res.end('error');
-//     })
-// }
+
 app.listen(5000, () => console.log("Server Started ..."));
--- src/components/inputBox/Upload.js
+++ src/components/inputBox/Upload.js
@@ -13,40 +13,42 @@ import {
 } from "../styledComponents/uploadStyle";
 
 
+
 const Upload = () => {
-    const [value, setValue] = useState('');
-    const [username, setUsername] = useState("");
-    const [imageName, setImageName] = useState("");
-    const [dataFile, setDataFile] = useState("");
     const [uploaded, setUploaded] = useState();
     const [selectedFile, setSelectedFile] = useState(null);
     const filePicker = useRef(null);
+    const hostRrl = "/upload";
 
     function handleChange(event) {
         console.log('upload file work ');
         let target = event.target || event.srcElement || event.currentTarget;
         console.log(target.files);
-        setSelectedFile(target.files[0])
+        setSelectedFile(target.files[0]);
 
     }
 
-    const handelUpload = async (e) => {
-        e.preventDefault();
+    const handelUpload = async () => {
         if (!selectedFile) {
             alert("please select a file");
             return;
         }
         const formData = new FormData();
         formData.append("file", selectedFile);
-        const result = await fetch({
-            method: "POST",
-            body: formData,
+        const res = await fetch(hostRrl,{
+            method:"POST",
+            headers : {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body:formData,
         });
-        const data = await result.json();
+        const data = await res.json();
+
         setUploaded(data);
 
-    }
 
+    }
 
 
     const handlePick = () => {
@@ -78,13 +80,6 @@ const Upload = () => {
                 <Wrapper>
                     <Title>Send Files in Server </Title>
                     <Form onSubmit={(e) => e.preventDefault()}>
-                        <Input type={"text"} placeholder={'imageName'} name="image"
-                               onChange={(e) => setValue(e.target.value)}/>
-                        <Input type={"hidden"} name="imagename" placeholder={""} value={""}
-                               onChange={(e) => setImageName(e.target.value)}/>
-                        <Input placeholder={'username'} type={"text"} onChange={(e) => setUsername(e.target.value)}
-                               name={"username"} value={""}/>
-
 
                         <ButtonUpload className="hidden" id="upload" type="file" onChange={handleChange}
                                       ref={filePicker}
